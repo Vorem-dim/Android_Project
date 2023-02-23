f913d774e5d251241d6988e010b3406635ead2d2
Vorem-dim <ustinov.artem2002@gmail.com>
2023-02-24 00:12:00 +0300

    Code initialization

--- app/src/main/java/com/example/sushiandpizza/MainActivity.java
+++ app/src/main/java/com/example/sushiandpizza/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.sushiandpizza;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView text = (TextView)findViewById(R.id.Name_cafe);
+        String str = getResources().getString(R.string.app_name);
+        text.setText(str);
+
+        ImageView image = (ImageView)findViewById(R.id.Pizza_main);
+        Drawable draw = getResources().getDrawable(R.drawable.pizza_main_screen);
+        image.setImageDrawable(draw);
+
+        image = (ImageView)findViewById(R.id.Sushi_main);
+        draw = getResources().getDrawable(R.drawable.sushi_main_screen);
+        image.setImageDrawable(draw);
     }
 }
